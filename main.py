ab2e658dd71e405e2e57c961bd2a0541ed920fe4
aandrjuscenko <125612118+aandrjuscenko@users.noreply.github.com>
2023-03-20 00:10:26 +0200

    lab_4

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-# python3
-
 def parallel_processing(n, m, data):
     output = []
+    atm=[0]*n
+    for i in data:
+      output.append([atm.index(min(atm)),min(atm)])
+      atm[atm.index(min(atm))]+=i 
+     
+      
     # TODO: write the function for simulating parallel tasks, 
     # create the output pairs
 
@@ -15,14 +19,19 @@ def main():
     # m - job count
     n = 0
     m = 0
+    ievade = list(map(int,(input().split())))
+    n=ievade[0]
+    m=ievade[1]
+    data=list(map(int,(input().split())))
+  
 
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
-    data = []
 
     # TODO: create the function
     result = parallel_processing(n,m,data)
-    
+    for i in result:
+        print(i[0],i[1])
     # TODO: print out the results, each pair in it's own line
 
 
